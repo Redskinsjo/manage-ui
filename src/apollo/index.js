50bef5051a39e24b7ef2b674bd9dfb8d8ecf9836
Jonathan Carnos <jonathancarnos@MacBook-Pro-de-Jonathan.local>
2021-10-02 20:57:12 +0200

    test

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -3,7 +3,7 @@ import AppWrapper from "../redux/Wrapper";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 const devUri = "http://localhost:4000/graphql";
 const prodUri = `${
-  process.env.PROD_GRAPHQL_API || "https://manage-graphql-api.herokuapp.com/"
+  process.env.PROD_GRAPHQL_API || "https://manage-graphql-api.herokuapp.com"
 }/graphql`;
 const client = new ApolloClient({
   uri: process.env.NODE_ENV === "development" ? devUri : prodUri,
