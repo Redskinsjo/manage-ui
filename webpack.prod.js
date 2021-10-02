54f1cce46ab44589b549e07da0bbd4027f63bf86
Jonathan Carnos <jonathancarnos@MacBook-Pro-de-Jonathan.local>
2021-10-02 19:12:55 +0200

    source map

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,10 @@
 const { merge } = require("webpack-merge");
+const path = require("path");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-// const webpack = require("webpack");
 
-module.exports = merge(common, {
+module.exports = {
   mode: "production",
   devtool: "source-map",
   plugins: [
@@ -20,4 +20,39 @@ module.exports = merge(common, {
     //   "process.env.NODE_ENV": JSON.stringify("development"),
     // }),
   ],
-});
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+};
